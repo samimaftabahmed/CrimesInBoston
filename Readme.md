0c2229fc179ccedc8c351c2cc557f18dfb6b1fe7
Samim <samimaftabahmed@gmail.com>
2019-09-23 10:09:35 +0530

    First Commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Crimes In Boston
+
+- Map Reduce program to analyse to crime rates in Boston
+
+For dataset visit: https://www.kaggle.com/AnalyzeBoston/crimes-in-boston
---
+++ src/main/java/com/samhad/App.java
@@ -0,0 +1,16 @@
+package com.samhad;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+
+public class App extends Configured implements Tool {
+
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public int run(String[] args) throws Exception {
+        return 0;
+    }
+}
